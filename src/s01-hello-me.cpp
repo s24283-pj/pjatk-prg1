196fb441bf9b3e6b1f72bcff7e08ad95ca73acd7
Czarek <s24283@pjwstk.edu.pl>
2021-11-02 22:06:07 +0100

    adding resolved tasks

---
+++ src/s01-hello-me.cpp
@@ -0,0 +1,13 @@
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+
+#include <iostream>
+
+std :: string name = "Cezary";
+auto main() -> int
+{
+    std::cout << "Hello "<<name<< std :: endl;
+    return 0;
+}
